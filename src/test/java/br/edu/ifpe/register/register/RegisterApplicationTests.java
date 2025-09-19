9a7c3046e7eb1723e9dc67ca16458f4bffa227a7
jadeilson <jadeilsonm17@gmail.com>
2025-09-19 16:22:45 -0300

    chore: adjustment in pipiline

--- src/test/java/br/edu/ifpe/register/register/RegisterApplicationTests.java
+++ src/test/java/br/edu/ifpe/register/register/RegisterApplicationTests.java
@@ -1,13 +1,8 @@
 package br.edu.ifpe.register.register;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RegisterApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
