9d4862d847f04d323b991d55f64efa1b8b6e03ca
jadeilson <jadeilsonm17@gmail.com>
2025-10-13 13:52:57 -0300

    feat(auth): refactor authentication service and update security filter

--- src/main/java/br/edu/ifpe/register/register/dto/LoginDTO.java
+++ src/main/java/br/edu/ifpe/register/register/dto/LoginDTO.java
@@ -1,6 +1,5 @@
 package br.edu.ifpe.register.register.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +11,4 @@ public class LoginDTO {
     private String email;
     @Size(min = 8, max = 250, message = "The password must be between 8 and 150 characters long.")
     private String password;
-
 }
